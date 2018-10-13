d621d2f08ceb22b81550f0d2bc4a78c3c87fc91e
Dustin Woods <pyrozzcom@gmail.com>
2018-10-12 22:30:08 -0700

    Maintain a list of jumpable bodies

--- Tools/Patrol.gd
+++ Tools/Patrol.gd
@@ -9,15 +9,18 @@ export (bool) var being_controlled = false
 
 onready var path_follow : PathFollow2D = get_node("PathFollow2D") 
 onready var path : Path2D = get_node("Path2D")
+onready var static_body : StaticBody2D = get_node("PathFollow2D/StaticBody")
+onready var jump_area : Area2D = get_node("PathFollow2D/JumpArea")
 
 var collision_circle : CircleShape2D = null
 var preview_offset = 0
+var jumpable_bodies : Array = Array()
 
 func _ready():
 	
 	collision_circle = CircleShape2D.new()
 	collision_circle.radius = jump_radius
-	$PathFollow2D/Area2D/CollisionShape2D.shape = collision_circle
+	$PathFollow2D/JumpArea/CollisionShape2D.shape = collision_circle
 	
 	if path != null:
 		remove_child(path_follow)
@@ -37,6 +40,7 @@ func _process(delta):
 # Actual game movement
 func process_movement(delta):
 	path_follow.offset += delta * move_speed
+	jump_area.monitoring = being_controlled
 
 # Process for just tooling around
 func process_tool(delta):
@@ -53,8 +57,14 @@ func _draw():
 	if Engine.is_editor_hint():
 		draw_circle(path_follow.position, jump_radius, Color(0.5, 0.5, 0, 0.5))
 
-func _on_Area2D_area_entered(area: Area2D):
-	print(area.name)
+func _on_JumpArea_body_entered(body : PhysicsBody2D):
+	if body != self.static_body:
+		var index = jumpable_bodies.find(body)
+		if index == -1:
+			jumpable_bodies.append(body)
 
-func _on_Area2D_area_exited(area):
-	pass # Replace with function body.
+func _on_JumpArea_body_exited(body):
+	if body != self.static_body:
+		var index = jumpable_bodies.find(body)
+		if index != -1:
+			jumpable_bodies.remove(index)
