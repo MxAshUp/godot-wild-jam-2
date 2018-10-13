4db4f9b4147ba4a76017cacf60aeb6e7a2fd6cac
Dustin Woods <pyrozzcom@gmail.com>
2018-10-12 21:47:23 -0700

    Added circle area for detection jump allowed

--- Tools/Patrol.gd
+++ Tools/Patrol.gd
@@ -9,15 +9,21 @@ export (bool) var being_controlled = false
 
 onready var path_follow : PathFollow2D = get_node("PathFollow2D") 
 onready var path : Path2D = get_node("Path2D")
-var circle_area_shape : CircleShape2D = null
 
+var collision_circle : CircleShape2D = null
 var preview_offset = 0
 
 func _ready():
+	
+	collision_circle = CircleShape2D.new()
+	collision_circle.radius = jump_radius
+	$PathFollow2D/Area2D/CollisionShape2D.shape = collision_circle
+	
 	if path != null:
 		remove_child(path_follow)
 		path.add_child(path_follow)
 		path_follow.offset = start_offset
+	
 	else:
 		set_process(false)
 
@@ -28,16 +34,27 @@ func _process(delta):
 	else:
 		process_movement(delta)
 
+# Actual game movement
 func process_movement(delta):
 	path_follow.offset += delta * move_speed
 
+# Process for just tooling around
 func process_tool(delta):
-	preview_offset += delta * move_speed
-	if preview:
-		path_follow.offset = preview_offset + start_offset
-		update()
-	else:
-		path_follow.offset = start_offset
+	if path_follow != null:
+		preview_offset += delta * move_speed
+		if preview:
+			path_follow.offset = preview_offset + start_offset
+			update()
+		else:
+			path_follow.offset = start_offset
 
 func _draw():
-	draw_circle(path_follow.position, jump_radius, Color(0.5, 0.5, 0, 0.5))
+	# Show radius of jump
+	if Engine.is_editor_hint():
+		draw_circle(path_follow.position, jump_radius, Color(0.5, 0.5, 0, 0.5))
+
+func _on_Area2D_area_entered(area: Area2D):
+	print(area.name)
+
+func _on_Area2D_area_exited(area):
+	pass # Replace with function body.
