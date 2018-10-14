f33bd3a30ac4717233b308272063b266f96870a9
Dustin Woods <pyrozzcom@gmail.com>
2018-10-13 21:27:20 -0700

    Guard now follows patrol path point

--- LevelComponents/Guard.gd
+++ LevelComponents/Guard.gd
@@ -1,2 +1,27 @@
 extends "Jumpable.gd"
+const Patrol = preload("Patrol.gd")
+var patrol : Node
+var patrolling : bool = false
+var position_follow : Position2D
+var go_to_position : Vector2 = self.position
+var velocity : Vector2 = Vector2()
+export var max_speed = 100
 
+func _ready():
+	patrol = get_parent()
+	if patrol is Patrol:
+		patrolling = true
+	
+func _process(delta):
+	if position_follow:
+		var patrol_position = position_follow.global_position
+		go_to_position = Vector2(patrol_position.x, patrol_position.y)
+	elif patrol:
+		position_follow = (patrol as Patrol).get_position_follow()
+
+func _physics_process(delta):
+	if go_to_position:
+		var follow_vector = go_to_position - self.position
+		velocity = follow_vector.normalized() * max_speed 
+		velocity = move_and_slide(velocity)
+		
--- LevelComponents/Patrol.gd
+++ LevelComponents/Patrol.gd
@@ -4,22 +4,25 @@ extends Node2D
 export (float) var move_speed = 100
 export (float) var start_offset = 0
 export (bool) var preview = false
-
+var preview_offset = 0
+	
+onready var position_follow : Position2D = get_node("PathFollow2D/Position2D") 
 onready var path_follow : PathFollow2D = get_node("PathFollow2D") 
 onready var path : Path2D = get_node("Path2D")
-#onready var static_body : StaticBody2D = get_node("PathFollow2D/StaticBody")
-#onready var indicator_sprite : Sprite = get_node("PathFollow2D/IndicatorSprite")
 
-var preview_offset = 0
+signal out_of_range
 
 func _ready():
 	if path != null:
-		remove_child(path_follow)
+		self.remove_child(path_follow)
 		path.add_child(path_follow)
 		path_follow.offset = start_offset
-	
 	else:
 		set_process(false)
+
+
+func get_position_follow():
+	return position_follow
 
 
 func _process(delta):
@@ -30,7 +33,8 @@ func _process(delta):
 		process_movement(delta)
 	
 	if path:
-		print(Vector2(path_follow.h_offset, path_follow.v_offset))
+		pass
+		#print(Vector2(path_follow.h_offset, path_follow.v_offset))
 
 
 # Actual game movement
@@ -47,3 +51,4 @@ func process_tool(delta):
 			update()
 		else:
 			path_follow.offset = start_offset
+
