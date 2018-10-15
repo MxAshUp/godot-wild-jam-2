754a19cca82b6820542d0b2a1572834d88a73018
Zach777 <themanofthehour7778@gmail.com>
2018-10-15 15:40:28 -0400

    Created pillars for Doors

--- LevelComponents/Door.gd
+++ LevelComponents/Door.gd
@@ -1,17 +1,15 @@
-extends StaticBody2D
+extends Node2D
 
 export var door_open : bool = false
 
 signal interact
 
+
+
 func _on_Door_interact():
 	if door_open :
 		#Close door
-		self.set_collision_layer_bit( 0, true )
-		$Col.show()
 		door_open = false
 	else:
 		#open door
-		self.set_collision_layer_bit( 0, false )
-		$Col.hide()
 		door_open = true
