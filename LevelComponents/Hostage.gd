6156c4a4713aa81285091191e545e6c671c30dac
Dustin Woods <pyrozzcom@gmail.com>
2018-10-16 21:25:52 -0700

    Changed hostage movement physics

--- LevelComponents/Hostage.gd
+++ LevelComponents/Hostage.gd
@@ -1,26 +1,42 @@
 tool
 extends "Jumpable.gd"
 
-export var run_speed = 200
 
 signal rescued #When the hostage is rescued, this gets emitted.
 signal hostage_caught
 
+var velocity : Vector2 = Vector2()
+const MAX_SPEED = 200
+const ACCELERATION = 1400
+const FRICTION = 1500
 
 func _process( delta ):
 	
 	$SpiritParticles.emitting = being_controlled
 	
+	var move : Vector2 = Vector2( 0,0 )
+	
 	if being_controlled :
 	#Get WASD movement
-		var move : Vector2 = Vector2( 0,0 )
 		move.x += int( Input.is_action_pressed( "ui_right" ) )
 		move.x += -int( Input.is_action_pressed( "ui_left" ) )
 		move.y += int( Input.is_action_pressed( "ui_down" ) )
 		move.y += -int( Input.is_action_pressed( "ui_up" ) )
 		move = move.clamped( 1 )
-		
-		move_and_slide( move * run_speed )
+
+	if move.length() > 0:
+		velocity += move * ACCELERATION * delta
+	else:
+		var speed = velocity.length()
+		speed -= FRICTION * delta
+		speed = max(0, speed)
+		velocity = velocity.normalized() * speed
+
+	if velocity.length() > MAX_SPEED:
+		velocity = velocity.normalized() * MAX_SPEED
+	
+	velocity = move_and_slide(velocity)
+
 
 
 func _ready():
@@ -32,7 +48,7 @@ func _ready():
 
 
 func rescued():
-	#Begin playing animations and victory sounds etc.
+	# Begin playing animations and victory sounds etc.
 	pass
 
 
