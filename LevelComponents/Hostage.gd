66d9b332d153f384f6c8718fce9a34db7fbc9c7b
Dustin Woods <pyrozzcom@gmail.com>
2018-10-19 20:32:05 -0700

    hostage animations

--- LevelComponents/Hostage.gd
+++ LevelComponents/Hostage.gd
@@ -6,6 +6,7 @@ signal rescued #When the hostage is rescued, this gets emitted.
 signal hostage_caught
 
 var velocity : Vector2 = Vector2()
+var last_move_dir = 0.0
 const MAX_SPEED = 200
 const ACCELERATION = 1900
 const FRICTION = 2600
@@ -37,7 +38,50 @@ func _process( delta ):
 	
 	velocity = move_and_slide(velocity)
 
+	process_animation(delta)
 
+func process_animation(delta):
+	var animation_to_play = $AnimationPlayer.current_animation
+	$AnimationPlayer.playback_speed = 1
+
+	var left_right_move = false
+	
+	if animation_to_play == "walk_lr":
+		animation_to_play = "idle_lr"
+	elif animation_to_play == "walk_up":
+		animation_to_play = "idle_up"
+	elif animation_to_play == "walk_down":
+		animation_to_play = "idle_down"
+	
+
+	if abs(velocity.x) > abs(velocity.y):
+		if velocity.x < 0:
+			last_move_dir = -1.0
+	
+		if velocity.x > 0:
+			last_move_dir = 1.0
+		
+		if abs(velocity.x) > 1.0:
+			animation_to_play = "walk_lr"
+	else:
+	
+		if velocity.y > 1.0:
+			animation_to_play = "walk_down"
+		
+		if velocity.y < -1.0:
+			animation_to_play = "walk_up"
+		
+	$AnimationPlayer.playback_speed = (velocity.length() / MAX_SPEED) * 1.5
+		
+	if animation_to_play != $AnimationPlayer.current_animation:
+		$AnimationPlayer.play(animation_to_play)
+	
+	# maybe flip sprite, if supported (ending with _lr)
+	if animation_to_play.ends_with('_lr'):
+		if last_move_dir < 0:
+			$CharacterSprite.flip_h = true
+		elif last_move_dir > 0:
+			$CharacterSprite.flip_h = false
 
 func _ready():
 	if being_controlled :
