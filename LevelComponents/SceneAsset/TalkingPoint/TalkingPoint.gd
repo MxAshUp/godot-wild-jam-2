619676d13f7c6bbd2f6b78250219beb5caa7a72a
Zach777 <themanofthehour7778@gmail.com>
2018-10-20 11:05:17 -0400

    Had to talking points individually.

---
+++ LevelComponents/SceneAsset/TalkingPoint/TalkingPoint.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+
+#When two talking points collide and they
+#have the same talking script, they will play
+#the talking script.
+
+
+export var talk_scripts : PoolStringArray = PoolStringArray( [] )
+
+
+#First talker to collide turns the other's can_play_talk off.
+var can_play_talk = true
+
+
+func _ready():
+	self.connect( "area_entered", self, "attempt_talk" )
+
+
+func attempt_talk( area ):
+	area.can_play_talk = false
+	
+	#If both talkers have the same talking point, start playing it.
+	
+	for talk in talk_scripts :
+		for check_talk in area.talk_scripts :
+			if talk == check_talk :
+				#Play the talk script and exit loop.
+				var talk_match = Music.get_talk( talk )
+				$Talker.stream = talk_match
+				$Talker.play()
+
+
